2be06500942e1f333ab9b8ddc81aef22f5a279ec
BaTru <gietbarca@gmail.com>
2017-09-11 14:08:44 +0700

    list of category

---
+++ app/src/main/java/adapter/ListCategoryAdapter.java
@@ -0,0 +1,54 @@
+package adapter;
+
+import android.content.Context;
+import android.media.Image;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.*;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.gietb.banhangkhoapham.R;
+
+public class ListCategoryAdapter extends android.support.v4.view.PagerAdapter {
+
+    private Context context;
+    private LayoutInflater inflater;
+    private Integer[] images = {R.drawable.little, R.drawable.maxi, R.drawable.midi, R.drawable.fit, R.drawable.mini, R.drawable.party};
+
+    public ListCategoryAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.custom_category, null);
+        ImageView imageView = view.findViewById(R.id.imageView);
+        imageView.setImageResource(images[position]);
+
+        ViewPager viewPager = (ViewPager) container;
+        viewPager.addView(view, 0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ViewPager viewPager = (ViewPager) container;
+        View view = (View) object;
+        viewPager.removeView(view);
+    }
+}
--- app/src/main/java/main_fragment/HomeFragment.java
+++ app/src/main/java/main_fragment/HomeFragment.java
@@ -3,18 +3,24 @@ package main_fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.gietb.banhangkhoapham.R;
 
+import adapter.ListCategoryAdapter;
+
 public class HomeFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ViewPager pager = view.findViewById(R.id.pagerCategory);
+        ListCategoryAdapter adapter = new ListCategoryAdapter(view.getContext());
+        pager.setAdapter(adapter);
+        return view;
     }
 }

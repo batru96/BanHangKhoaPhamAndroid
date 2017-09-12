d5cb38f0c01bb6fed5cf76c8d156790f747533d1
BaTru <gietbarca@gmail.com>
2017-09-12 17:04:21 +0700

    working on sign_in_out

---
+++ app/src/main/java/com/example/gietb/banhangkhoapham/SignInOutActivity.java
@@ -0,0 +1,15 @@
+package com.example.gietb.banhangkhoapham;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SignInOutActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_out);
+    }
+}

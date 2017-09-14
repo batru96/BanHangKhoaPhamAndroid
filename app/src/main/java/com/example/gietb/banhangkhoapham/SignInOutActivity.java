53d4e255199740e7a3f621bad633af9064763293
BaTru <gietbarca@gmail.com>
2017-09-14 08:04:24 +0700

    Go to sign in up activity

--- app/src/main/java/com/example/gietb/banhangkhoapham/SignInOutActivity.java
+++ app/src/main/java/com/example/gietb/banhangkhoapham/SignInOutActivity.java
@@ -3,18 +3,21 @@ package com.example.gietb.banhangkhoapham;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import sign_up_fragment.SignInFragment;
 import sign_up_fragment.SignUpFragment;
 
 public class SignInOutActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btnSignIn, btnSignUp;
+    private ImageButton btnBack;
     private FragmentTransaction fragmentTransaction;
 
     @Override
@@ -28,10 +31,21 @@ public class SignInOutActivity extends AppCompatActivity implements View.OnClick
     private void initControls() {
         btnSignIn = findViewById(R.id.signInButton);
         btnSignUp = findViewById(R.id.signUpButton);
+        btnBack = findViewById(R.id.backToMainButton);
 
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                backToMain();
+            }
+        });
         btnSignIn.setOnClickListener(this);
         btnSignUp.setOnClickListener(this);
         onClick(btnSignIn);
+    }
+
+    private void backToMain() {
+        startActivity(new Intent(this, MainActivity.class));
     }
 
     @Override
--- app/src/main/java/drawer/SignInDrawerFragment.java
+++ app/src/main/java/drawer/SignInDrawerFragment.java
@@ -2,6 +2,7 @@ package drawer;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import com.example.gietb.banhangkhoapham.R;
+import com.example.gietb.banhangkhoapham.SignInOutActivity;
 
 public class SignInDrawerFragment extends Fragment {
     private ISendButton listener;
@@ -28,7 +30,8 @@ public class SignInDrawerFragment extends Fragment {
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                listener.sendButton(btnSignIn);
+                startActivity(new Intent(getActivity(), SignInOutActivity.class));
+                //listener.sendButton(btnSignIn);
             }
         });
     }
